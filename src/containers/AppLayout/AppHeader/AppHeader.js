5c6e4d175a9e83c51224499f607934ac30adc7b7
phamtrungkien0506 <phamtrungkien0506@gmail.com>
2020-10-29 09:42:28 +0700

    ok

--- src/containers/AppLayout/AppHeader/AppHeader.js
+++ src/containers/AppLayout/AppHeader/AppHeader.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Link, AppBar, Grid } from '@material-ui/core';
-import { ExpandMore, AccountCircle } from '@material-ui/icons';
+import {
+  Box,
+  Link,
+  Grid,
+  AppBar,
+  IconButton,
+} from '@material-ui/core';
+import { Menu, ExpandMore, AccountCircle } from '@material-ui/icons';
 import { useHistory } from 'react-router-dom';
 
 import toJs from '../../../hoc/ToJS';
@@ -26,31 +32,77 @@ function AppHeader(props) {
         position="fixed"
         style={{ backgroundColor: '#39424E', padding: '0 20px' }}
       >
-        <Grid container justify="center" style={{ height: 60 }}>
-          <Grid item lg={5} sm={6}>
+        <Grid container justify="center" alignItems="center" style={{ height: 60 }}>
+          {/*  Mobile  */}
+          <Grid
+            item
+            xs={2}
+            component={Box}
+            display={{ xs: 'block', sm: 'none' }}
+          >
+            <IconButton style={{ color: 'white' }}>
+              <Menu />
+            </IconButton>
+          </Grid>
+          <Grid
+            item
+            xs={8}
+            component={Box}
+            display={{ xs: 'block', sm: 'none' }}
+          >
+            <Link href={ROUTER.HOME}>
+              <img src={code} alt="logo" height="36px" style={{ display: 'block', margin: '0 auto' }} />
+            </Link>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            component={Box}
+            display={{ xs: 'block', sm: 'none' }}
+          >
+            <IconButton style={{ float: 'right', color: 'white' }}>
+              <AccountCircle />
+            </IconButton>
+          </Grid>
+
+          {/*  Desktop  */}
+          <Grid
+            item
+            sm={6}
+            lg={5}
+            component={Box}
+            display={{ xs: 'none', sm: 'block' }}
+          >
             <div style={{ display: 'flex', flexDirection: 'row' }}>
               <Link href={ROUTER.HOME}>
-                <img src={code} alt="logo" height="36px" style={{ padding: '10px 0' }} />
+                <img src={code} alt="logo" height="36px" style={{ padding: '12px 0' }} />
               </Link>
               <div
                 style={{
+                  height: 55,
+                  margin: '0 12px',
+                  padding: '0 8px',
                   cursor: 'pointer',
-                  margin: '0 24px',
-                  padding: '18px 6px 0',
-                  borderBottom: 'solid 5px #1BA94C',
+                  lineHeight: '60px',
+                  borderBottom: '5px solid #1BA94C',
                 }}
               >
                 Bài tập
               </div>
             </div>
           </Grid>
-          <Grid item lg={5} sm={6}>
+          <Grid
+            item
+            sm={6}
+            lg={5}
+            component={Box}
+            display={{ xs: 'none', sm: 'block' }}
+          >
             <div
               style={{
                 cursor: 'pointer',
                 display: 'flex',
                 float: 'right',
-                padding: '18px 0',
               }}
             >
               <AccountCircle style={{ padding: '0 4px' }} />
