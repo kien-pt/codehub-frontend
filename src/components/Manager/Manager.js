cfcf0b170ced44a3f9fe39834e63cc16926139d0
Phạm Trung Kiên <phamtrungkien0506@gmail.com>
2020-12-03 19:43:22 +0700

    manager-profile

--- src/components/Manager/Manager.js
+++ src/components/Manager/Manager.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
+import Profile from '../../containers/Manager/Profile';
+
 function Manager() {
   return (
-    <div>ok</div>
+    <Grid container justify="center" spacing={3}>
+      <Grid item lg={3} md={4} xs={12}>
+        <Profile />
+      </Grid>
+      <Grid item lg={7} md={8} xs={12}>
+
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/containers/Manager/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+function Profile() {
+  return (
+    <div>ok</div>
+  );
+}
+
+export default Profile;
