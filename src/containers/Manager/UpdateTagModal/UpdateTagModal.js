bd4021e5ec6cedb4fbf42148b04d2c949df01e8f
Phạm Trung Kiên <phamtrungkien0506@gmail.com>
2020-12-10 14:58:31 +0700

    clean-code

--- src/containers/Manager/UpdateTagModal/UpdateTagModal.js
+++ src/containers/Manager/UpdateTagModal/UpdateTagModal.js
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import {
+  Button,
+  Snackbar,
   FormControl,
   OutlinedInput,
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
-  Button,
-  Snackbar,
-  FormControlLabel,
-  Checkbox,
 } from '@material-ui/core';
 import { Alert } from '@material-ui/lab'; 
-import { AssignmentTurnedIn, AssignmentLate } from '@material-ui/icons';
+import { AssignmentLate } from '@material-ui/icons';
 
 import toJs from '../../../hoc/ToJS';
-import select from '../../../utils/select';
-import ROUTER from '../../../constant/router';
 
 import { updateTag } from '../../../reducer/quiz';
 
@@ -50,12 +45,7 @@ function UpdateTagModal(props) {
 
   return (
     <>
-      <Dialog
-        open={updatedTag != null}
-        keepMounted
-        onClose={() => setUpdatedTag(null)}
-        style={{ minWidth: 640 }}
-      >
+      <Dialog open={updatedTag != null} keepMounted onClose={() => setUpdatedTag(null)} style={{ minWidth: 640 }}>
         <DialogTitle>Sửa danh mục khoá học</DialogTitle>
         <DialogContent style={{ marginTop: 12 }}>
           <form onSubmit={handleSubmit}>
@@ -73,26 +63,19 @@ function UpdateTagModal(props) {
           </form>
         </DialogContent>
         <DialogActions style={{ padding: '0 24px 12px 24px' }}>
-          <Button variant="outlined" onClick={() => setUpdatedTag(null)} color="secondary">
-            Huỷ
-          </Button>
-          <Button variant="contained" onClick={handleSubmit} color="primary">
-            Đồng ý
-          </Button>
+          <Button variant="outlined" color="secondary" onClick={() => setUpdatedTag(null)}>Huỷ</Button>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>Đồng ý</Button>
         </DialogActions>
       </Dialog>
 
       <Snackbar open={noti !== null} autoHideDuration={6000} onClose={() => setNoti(null)}>
-        <Alert variant="filled" severity={noti?.type} onClose={() => setNoti(null)}>
-          {noti?.message}
-        </Alert>
+        <Alert variant="filled" severity={noti?.type} onClose={() => setNoti(null)}>{noti?.message}</Alert>
       </Snackbar>
     </>
   );
 }
 
-const mapStateToProps = (state) => ({
-});
+const mapStateToProps = () => ({});
 
 const mapDispatchToProps = (dispatch) => ({
   updateTag: (payload) => dispatch(updateTag(payload)),
