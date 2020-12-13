0c1386feb38caa65b3c13cc80dcce83122a34ae9
Phạm Trung Kiên <phamtrungkien0506@gmail.com>
2020-12-13 23:58:03 +0700

    clean-code

--- src/containers/InsertQuiz/InsertQuizPreviewModal/InsertQuizPreviewModal.js
+++ src/containers/InsertQuiz/InsertQuizPreviewModal/InsertQuizPreviewModal.js
@@ -5,14 +5,11 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
 } from '@material-ui/core';
 import Interweave from 'interweave';
 
-
 import toJs from '../../../hoc/ToJS';
-import select from '../../../utils/select';
 
 function InsertQuizPreviewModal(props) {
   const { combine, isPreviewing, setPreviewing } = props;
@@ -20,21 +17,15 @@ function InsertQuizPreviewModal(props) {
   return (
     <Dialog open={isPreviewing} onClose={() => setPreviewing(false)}>
       <DialogTitle>Xem trước</DialogTitle>
-      <DialogContent>
-        <Interweave content={combine} />
-      </DialogContent>
-      <DialogActions>
-        <Button variant="contained" color="primary" onClick={() => setPreviewing(false)}>Đóng</Button>
-      </DialogActions>
+      <DialogContent><Interweave content={combine} /></DialogContent>
+      <DialogActions><Button variant="contained" color="primary" onClick={() => setPreviewing(false)}>Đóng</Button></DialogActions>
     </Dialog>
   );
 }
 
-const mapStateToProps = (state) => ({
-});
+const mapStateToProps = () => ({});
 
-const mapDispatchToProps = (dispatch) => ({
-});
+const mapDispatchToProps = () => ({});
 
 export default connect(
   mapStateToProps,
--- src/containers/Submission/SubmissionDetail/SubmissionDetail.js
+++ src/containers/Submission/SubmissionDetail/SubmissionDetail.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  Button,
   Grid,
-  Divider,
-  Table,
-  TableContainer,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
+  Button,
+  Card,
+  CardHeader,
+  CardContent,
 } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-import { CheckRounded, CloseRounded } from '@material-ui/icons';
 import {Controlled as CodeMirror} from 'react-codemirror2';
 
 import toJs from '../../../hoc/ToJS';
 import select from '../../../utils/select';
 import ROUTER from '../../../constant/router';
 
+require('codemirror/theme/neat.css');
 require('codemirror/lib/codemirror.css');
 require('codemirror/theme/material.css');
-require('codemirror/theme/neat.css');
 require('codemirror/mode/clike/clike.js');
 
 function SubmissionDetail(props) {
@@ -35,14 +27,9 @@ function SubmissionDetail(props) {
 
   const submission = props.submission?.find((e) => e.id === submissionId);
 
-  const quiz = submission?.quiz;
-
   return (
     <Card style={{ marginTop: 32 }}>
-      <CardHeader
-        title="Bài nộp"
-        style={{ color: 'white', backgroundColor: '#39424E' }}
-      />
+      <CardHeader title="Bài nộp" style={{ color: 'white', backgroundColor: '#39424E' }}/>
       <CardContent>
         <CodeMirror
           value={submission?.sourceCode}
@@ -79,8 +66,7 @@ const mapStateToProps = (state) => ({
   isFetching: select(state, 'quizReducer', 'isFetching'),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-});
+const mapDispatchToProps = () => ({});
 
 export default connect(
   mapStateToProps,
